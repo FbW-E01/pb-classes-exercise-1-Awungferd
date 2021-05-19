cb23d2567679bb32d4a830b1f4682402c6f4981a
Ferdinand <ndimbecha@gmail.com>
2021-05-19 02:16:08 +0200

    Get email and full name

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+// ### JS Classes
+
+// Create the instance properties `fullname` and `email` in the `Employee` class. Given a person's first and last names:
+
+// Form the `fullname` by simply joining the first and last name together, separated by a space.
+// Form the `email` by joining the first and last name together with a `.` in between, and follow it with `@company.com` at the end. Make sure the entire email is in lowercase.
+// Examples
+
 class Employee {
-	constructor (firstname, lastname) {
-		this.firstname = firstname
-		this.lastname = lastname
-		// Complete the code!
-	}
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+
+    // Complete the code!
+  }
+
 }
+
+const names = new Employee("John", "Devil");
+const mail = new Employee("Frances", "Abena");
+
+Employee.fullName = `${names.firstname}  ${names.lastname}`;
+Employee.email = `${mail.firstname}.${mail.lastname}@company.com`;
+
+console.log("Employee full name:", Employee.fullName);
+console.log("Employee Email:", Employee.email.toLowerCase());
